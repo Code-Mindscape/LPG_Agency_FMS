ac53b4ba2f69c9ba43d311ae7a0f44d627a4da89
Mirza Usman <mirzausmanmu07842@gmail.com>
2024-06-25 18:33:54 +0500

    added Shadcn UI

--- src/components/ActionsHeader/ActionsHeader.jsx
+++ src/components/ActionsHeader/ActionsHeader.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function ActionsHeader() {
+export default function ActionsHeader({tabName, ...props}) {
   return (
-    <div className='w-full h-'>
-
+    <div className='w-full h-20 bg-zinc-100/80 border flex items-center justify-center border-l-0 border-b-1 border-zinc-400/50'>
+      <div className="tabName w-[20%] flex items-center justify-center">
+          <h3 className='font-bold text-zinc-800 text-xl'>{tabName}</h3>
+      </div>
+      <div className="actionsBtn w-[80%] flex items-center justify-center">
+          
+      </div>
     </div>
   )
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
+import ActionsHeader from '../ActionsHeader/ActionsHeader'
 
 export default function Users() {
   return (
@@ -8,7 +9,10 @@ export default function Users() {
     <div className="users w-full h-screen flex-col flex items-center justify-center">
     <Navbar />
     <div className="mainContent flex-grow w-full bg-zinc-300">
-      <h1>Users</h1>
+      <ActionsHeader tabName={"All users"}/>
+      <div className="main w-full flex-grow flex items-center justify-center">
+        main
+      </div>
     </div>
     </div>
     </>
